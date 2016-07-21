2d05488e47bad37c58b678de0aa4b9e748856abf
abdifatah <abdifatah4@msn.com>
2016-07-21 11:15:34 +0200

    final projects

---
+++ js/main.js
@@ -0,0 +1,76 @@
+/*age
+var age=prompt('quel âge as tu');
+if(age > 18 && age < 80){
+  alert("majeur");
+}else if(age < 18 ){
+  alert("mineur");
+}
+else{
+  alert('donne moi un age valide connard');
+}
+*/
+
+/*
+var chiffre1=prompt('donne un chiffre');
+var chiffre2=prompt('donne un chiffre');
+alert(addition(chiffre1, chiffre2));
+
+
+
+function addition(chiffre1, chiffre2){
+  var result;
+  if (isNaN(chiffre1) || isNaN(chiffre2)){
+    result = 'recommence connard';
+  }else{
+    result = parseInt(chiffre1) + parseInt(chiffre2);
+  }
+
+  return result;
+}
+*/
+/*
+var chiffre1=prompt('donne un chiffre');
+var chiffre2=prompt('donne un chiffre');
+var choix = prompt('choisi');
+if (choix == '+') {
+  alert(addition(chiffre1, chiffre2));
+}
+else if (choix == '-'){
+  alert(soustraction(chiffre1, chiffre2));
+}
+//alert(addition(chiffre1, chiffre2));
+//alert(soustraction(chiffre1, chiffre2));
+
+
+
+function soustraction(chiffre1, chiffre2){
+  var result;
+  if (isNaN(chiffre1) || isNaN(chiffre2)){
+    result = 'recommence connard';
+  }else{
+    result = parseInt(chiffre1) - parseInt(chiffre2);
+  }
+
+  return result;
+}
+
+function addition(chiffre1, chiffre2){
+  var result;
+  if (isNaN(chiffre1) || isNaN(chiffre2)){
+    result = 'recommence connard';
+  }else{
+    result = parseInt(chiffre1) + parseInt(chiffre2);
+  }
+
+  return result;
+}
+*/
+
+
+// var prenom=prompt('donne ton prénom');
+// var age=prompt('donne ton âge');
+// alert(prenom +' '+ age);
+
+// for (i = 0; i <= 100; i ++){
+//   console.log(i);
+// }
